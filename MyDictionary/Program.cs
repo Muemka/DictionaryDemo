139245854e361e75ea507cb10b9974b1a2e2b745
Muhammed Emre KARAMAN <m.emre.karaman@outlook.com>
2021-02-26 16:23:51 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+
+        static Dictionary<string, int> kutuphane = new Dictionary<string, int>();
+
+
+        static void Main(string[] args)
+        {
+            kutuphane.Add("Emre", 27);
+            kutuphane.Add("Mustafa", 29);
+            kutuphane.Add("Ahmet", 45);
+            kutuphane.Add("Veli", 38);
+
+            foreach (var item in kutuphane)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
